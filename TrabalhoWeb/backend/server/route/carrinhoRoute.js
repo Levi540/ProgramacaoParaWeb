dc45ba0a4453f8a2d52944837c761cfa20a5f774
Gabriel Levi Dalfovo <59920991+Levi540@users.noreply.github.com>
2021-06-24 21:21:22 -0300

    route

---
+++ TrabalhoWeb/backend/server/route/carrinhoRoute.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const carrinhoService = require('../service/carrinhoService');
+
+router.post('/carrinho', async function(req, res) {
+    try {
+        const carrinho = req.body;
+        console.log(carrinho);
+        const newCarrinho = await carrinhoService.saveCarrinho(carrinho);
+        res.json(newCarrinho);
+    } catch (error) {
+        console.log(error);
+        res.json({messagem: error.message });
+    }
+});
+
+router.get('/carrinho/:id', async function (req, res) {
+    const carrinho = await carrinhoService.getBook(req.params.id);
+    res.json(carrinho);
+});
+
+module.exports = router;
