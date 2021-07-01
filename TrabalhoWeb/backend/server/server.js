0b7bd69e62408c444bd8d811dc66440c2895382d
Gabriel Levi Dalfovo <59920991+Levi540@users.noreply.github.com>
2021-06-30 21:20:16 -0300

    Update server.js

--- TrabalhoWeb/backend/server/server.js
+++ TrabalhoWeb/backend/server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 //const booksRoute = require('./route/booksRoute');
 const usersRoute = require('./route/usersRoute');
 const wineRoute = require('./route/wineRoute');
+const carrinhoRoute = require('./route/carrinhoRoute');
+const carrinhoItemRoute = require('./route/carrinhoItemRoute');
+const compraRoute = require('./route/compraRoute');
 const cors = require('cors');
 const app = express();
 
@@ -10,4 +13,7 @@ app.use(cors());
 //app.use(booksRoute);
 app.use(usersRoute);
 app.use(wineRoute);
+app.use(carrinhoRoute);
+app.use(carrinhoItemRoute);
+app.use(compraRoute);
 app.listen(3333);
